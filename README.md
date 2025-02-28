e441a2d0cfeab6030c79420a00b95059f2d1a26f
ricardocruz <ricardo.cruz@swau.edu>
2025-02-27 23:39:43 -0600

    Update README and updated currency converter functionality

--- README.md
+++ README.md
@@ -1,34 +1,45 @@
-## Usage
+# Currency Converter Chrome Extension
 
-Those templates dependencies are maintained via [pnpm](https://pnpm.io) via `pnpm up -Lri`.
+A Chrome extension that seamlessly converts currency values. Built with **SolidJS** for a highly responsive and efficient user interface. The extension provides accurate conversions based on the latest exchange rates.
 
-This is the reason you see a `pnpm-lock.yaml`. That being said, any package manager will work. This file can be safely be removed once you clone a template.
+## Features
 
-```bash
-$ npm install # or pnpm install or yarn install
-```
+- Automatically identifies and converts currency values on web pages.
+- Supports multiple currencies with up-to-date exchange rates from trusted APIs.
+- Customizable settings to choose your preferred currency.
+- Lightweight and fast, leveraging the power of SolidJS for a smooth user experience.
 
-### Learn more on the [Solid Website](https://solidjs.com) and come chat with us on our [Discord](https://discord.com/invite/solidjs)
+## Tech Stack
 
-## Available Scripts
+- **SolidJS**: Reactive framework for building the user interface.
+- **Chrome Extensions API**: Integration with the browser environment.
+- **Exchange Rate API (hexarate)**: [(or other API of your choice) for fetching real-time rates.](https://hexarate.paikama.co/)
 
-In the project directory, you can run:
+## Installation
 
-### `npm run dev` or `npm start`
+1. Clone this repository:
+    ```bash
+    git clone https://github.com/your-username/currency-converter-extension.git
+    ```
+2. Navigate to the project directory:
+    ```bash
+    cd currency-converter-extension
+    ```
+3. Install dependencies:
+    ```bash
+    npm install
+    ```
+4. Run Project:
+    ```bash
+    npm dev
+    ```
+5. Load the extension in Chrome:
+    - Open chrome://extensions in your browser.
+    - Enable Developer mode (top-right corner).
+    - Click on Load unpacked and select the dist folder from this project.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Contributing
+- Contributions are welcome! If you'd like to contribute, please fork the repository and submit a pull request.
 
-The page will reload if you make edits.<br>
-
-### `npm run build`
-
-Builds the app for production to the `dist` folder.<br>
-It correctly bundles Solid in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-## Deployment
-
-You can deploy the `dist` folder to any static host provider (netlify, surge, now, etc.)
+# License
+- This project is licensed under the MIT License.
--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,149 @@
-import { type Component, For, createSignal } from 'solid-js';
-import styles from './App.module.css';
-import { CurrencyConversionResponse, Currency } from './models/types';
+import { type Component, For, createSignal, onMount } from 'solid-js'
+import styles from './App.module.css'
+import { CurrencyConversionResponse, Currency } from './models/types'
 
-import Countries from '../src/countries.json';
+import Countries from '../src/countries.json'
 
-const countries: Currency[] = Countries;
+const countries: Currency[] = Countries
 
-let loading = false;
+let loading = false
 
 const fetchData = async (base: String, target: String) => {
-  loading = true;
-  const response = await fetch(`https://hexarate.paikama.co/api/rates/latest/${base}?target=${target}`);
-  const data = await response.json() as CurrencyConversionResponse;
-  loading = false;
-  return data;
+    loading = true
+    const response = await fetch(
+        `https://hexarate.paikama.co/api/rates/latest/${base}?target=${target}`
+    )
+    const data = (await response.json()) as CurrencyConversionResponse
+    loading = false
+    return data
 }
 
-
 const App: Component = () => {
-  const [baseCurrency, setBaseCurrency] = createSignal("USD");
-  const [targetCurrency, setTargetCurrency] = createSignal("EUR");
-  let amount = 1;
-  const [result, setResult] = createSignal(0);
+    const [baseCurrency, setBaseCurrency] = createSignal(
+        localStorage.getItem('baseCurrency') || 'USD'
+    )
+    const [targetCurrency, setTargetCurrency] = createSignal(
+        localStorage.getItem('targetCurrency') || 'EUR'
+    )
+    let amount = 1
+    const [result, setResult] = createSignal(0)
 
+    const convertCurrency = async () => {
+        const data = await fetchData(baseCurrency(), targetCurrency())
+        const conversionResult = data.data.mid * amount
+        setResult(conversionResult)
+        return result
+    }
 
-  const convertCurrency = async () => {
-    const data = await fetchData(baseCurrency(), targetCurrency());
-    const conversionResult = data.data.mid * amount;
-    setResult(conversionResult);
-    return result;
-  };
+    const flipCurrencies = () => {
+        const temp = baseCurrency()
+        setBaseCurrency(targetCurrency())
+        setTargetCurrency(temp)
+        convertCurrency()
+    }
 
-  const flipCurrencies = () => {
-    const temp = baseCurrency();
-    setBaseCurrency(targetCurrency());
-    setTargetCurrency(temp);
-  }
+    const setBaseCurrencyWithStorage = (value: string) => {
+        localStorage.setItem('baseCurrency', value)
+        setBaseCurrency(value)
+        convertCurrency()
+    }
 
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        {/* <img src={logo} class={styles.logo} alt="logo" /> */}
-        <h1>Currency Converter</h1>
-      </header>
-      <main>
-        <div class={styles.inputGroup}>
-          <label>
-            Amount:
-          </label>
-          <input 
-            type="number" 
-            step="0.01"
-            min="0"
-            value={amount} 
-            onInput={(e) => {
-              const value = e.currentTarget.value;
-              if (value && !isNaN(parseFloat(value))) {
-          amount = parseFloat(value);
-              }
-            }} 
-          />
+    const setTargetCurrencyWithStorage = (value: string) => {
+        localStorage.setItem('targetCurrency', value)
+        setTargetCurrency(value)
+        convertCurrency()
+    }
+
+    onMount(async () => {
+        await convertCurrency()
+    })
+
+    return (
+        <div class={styles.App}>
+            <header class={styles.header}>
+                {/* <img src={logo} class={styles.logo} alt="logo" /> */}
+                <h1>Currency Converter</h1>
+            </header>
+            <main>
+                <div class={styles.inputGroup}>
+                    <label>Amount:</label>
+                    <input
+                        type="number"
+                        step="0.01"
+                        min="0"
+                        value={amount}
+                        onInput={(e) => {
+                            const value = e.currentTarget.value
+                            if (value && !isNaN(parseFloat(value))) {
+                                amount = parseFloat(value)
+                            }
+                        }}
+                    />
+                </div>
+                <div class="grid grid-cols-2 gap-4">
+                    <div class={styles.inputGroup}>
+                        <label>Base Currency:</label>
+                        <select
+                            value={baseCurrency()}
+                            onChange={(e) =>
+                                setBaseCurrencyWithStorage(e.target.value)
+                            }
+                        >
+                            <For each={countries}>
+                                {(country) => (
+                                    <option value={country.code}>
+                                        {country.name}
+                                    </option>
+                                )}
+                            </For>
+                        </select>
+                    </div>
+                    <div class={styles.inputGroup}>
+                        <label>Target Currency:</label>
+                        <select
+                            value={targetCurrency()}
+                            onChange={(e) =>
+                                setTargetCurrencyWithStorage(e.target.value)
+                            }
+                        >
+                            <For each={countries}>
+                                {(country) => (
+                                    <option value={country.code}>
+                                        {country.name}
+                                    </option>
+                                )}
+                            </For>
+                        </select>
+                    </div>
+                </div>
+                {!loading && (
+                    <button
+                        class={styles.flipCurrency}
+                        onClick={flipCurrencies}
+                    >
+                        <i class="i-mdi:sync h-4 w-4"></i>
+                    </button>
+                )}
+                {!loading && (
+                    <button
+                        class={styles.submitButton}
+                        onClick={convertCurrency}
+                    >
+                        Convert
+                    </button>
+                )}
+                <div class="flex flex-col items-center">
+                    {result() === 0 ? (
+                        <div class="animate-spin i-mdi:loading h-6 w-6" />
+                    ) : (
+                        <h1 class="font-bold text-lg">
+                            Result: {result() + ' ' + targetCurrency()}
+                        </h1>
+                    )}
+                </div>
+            </main>
         </div>
-        <div class='grid grid-cols-2 gap-4'>
-          <div class={styles.inputGroup}>
-            <label>
-              Base Currency:
-            </label>
-            <select value={baseCurrency()} onChange={(e) => setBaseCurrency(e.target.value)}>
-                <For each={countries}>{(country) =>
-                <option value={country.code}>
-                  {country.name}
-                </option>
-                }</For>
-            </select>
-          </div>
-          <div class={styles.inputGroup}>
-            <label>
-              Target Currency:
-            </label>
-            <select value={targetCurrency()} onChange={(e) => setTargetCurrency(e.target.value)}>
-              <For each={countries}>{(country) =>
-                <option value={country.code}>
-                  {country.name}
-                </option>
-                }</For>
-            </select>
-          </div>
-        </div>
-        {!loading && <button class={styles.flipCurrency} onClick={flipCurrencies}><i class="i-mdi:sync h-4 w-4"></i></button>}
-        {!loading && <button class={styles.submitButton} onClick={convertCurrency}>Convert</button>}
-        <div class='flex flex-col items-center'>
-          <h1 class='font-bold text-lg'>Result: {result()}</h1>
-        </div>
-      </main>
-    </div>
-  );
-};
+    )
+}
 
-export default App;
+export default App
